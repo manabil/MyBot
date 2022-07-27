1868473985c1b0faa8dee32fc2e33bb201d14ccc
pureism <pureismx@gmail.com>
2022-07-27 07:59:54 +0700

    Changes scripts

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
-import os
-
-print('Testing python script')
+from http.server import BaseHTTPRequestHandler
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.end_headers()
+    self.wfile.write(self.headers.get('x-forwarded-for').encode())
+    return
